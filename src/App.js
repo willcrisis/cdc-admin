77cffa9efcf864eae672f93b8a1c66adb21b195f
Willian Krause <krause.willian@gmail.com>
2017-08-16 10:39:29 -0300

    List and save authors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,134 @@
 import React, {Component} from "react";
 import "./css/pure-min.css";
 import "./css/side-menu.css";
+import $ from "jquery";
 
 class App extends Component {
-  render() {
-    return (
-        <div id="layout">
-            <a href="#menu" id="menuLink" className="menu-link">
-                <span></span>
-            </a>
 
-            <div id="menu">
-                <div className="pure-menu">
-                    <a className="pure-menu-heading" href="#">Company</a>
+    constructor() {
+        super();
 
-                    <ul className="pure-menu-list">
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
-                    </ul>
+        this.state = {lista: [], nome: '', email: '', senha: ''};
+
+        this.submeter = this.submeter.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
+    }
+
+    componentDidMount() {
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/api/autores',
+            dataType: 'json',
+            success: dados => {
+                this.setState({lista: dados});
+            }
+        });
+    }
+
+    submeter(evento) {
+        evento.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8080/api/autores',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({nome: this.state.nome, email: this.state.email, senha: this.state.senha}),
+            success: dados => {
+                console.log('sucesso');
+            },
+            error: erro => {
+                console.log(erro);
+            }
+        });
+    }
+
+    setNome(evento) {
+        this.setState({nome: evento.target.value});
+    }
+
+    setEmail(evento) {
+        this.setState({email: evento.target.value});
+    }
+
+    setSenha(evento) {
+        this.setState({senha: evento.target.value});
+    }
+
+    render() {
+        return (
+            <div id="layout">
+                <a href="#menu" id="menuLink" className="menu-link">
+                    <span></span>
+                </a>
+
+                <div id="menu">
+                    <div className="pure-menu">
+                        <a className="pure-menu-heading" href="#">Company</a>
+
+                        <ul className="pure-menu-list">
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+                            <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div id="main">
+                    <div className="header">
+                        <h1>Home</h1>
+                    </div>
+
+                    <div className="content" id="content">
+                        <div className="pure-form pure-form-aligned">
+                            <form className="pure-form pure-form-aligned" onSubmit={this.submeter}>
+                                <div className="pure-control-group">
+                                    <label htmlFor="nome">Nome</label>
+                                    <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>
+                                </div>
+                                <div className="pure-control-group">
+                                    <label htmlFor="email">Email</label>
+                                    <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
+                                </div>
+                                <div className="pure-control-group">
+                                    <label htmlFor="senha">Senha</label>
+                                    <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>
+                                </div>
+                                <div className="pure-control-group">
+                                    <label></label>
+                                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div>
+                            <table className="pure-table">
+                                <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>E-mail</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    this.state.lista.map(autor => {
+                                        return (
+                                            <tr key={autor.id}>
+                                                <td>{autor.nome}</td>
+                                                <td>{autor.email}</td>
+                                            </tr>
+                                        );
+                                    })
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div id="main">
-                <div className="header">
-                    <h1>Home</h1>
-                </div>
-            </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
